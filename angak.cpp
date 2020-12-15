3916c328de5d9902f23c92f214ef542e1531764a
KanaeKocho <71975180+KanaeKocho@users.noreply.github.com>
2020-12-15 22:38:39 +0700

    simple , pke if

---
+++ angak.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int angka;
+    cout<< "=== Angka Jepang ==="<<endl;
+    cout<<"Masukkan Angka 0 - 10"<<endl;
+    cin>>angka;
+    if(angka == 0){
+        cout<<"零 (Zero)"<<endl;
+    }
+    if(angka == 1){
+        cout<<"一 (Ichi)"<<endl;
+    }
+    if(angka == 2){
+        cout<<"二 (Ni)"<<endl;
+    }
+    if(angka == 3){
+        cout<<"三 (San)"<<endl;
+    }
+    if(angka == 4){
+        cout<<"四 (Yan / Shi)"<<endl;
+    }
+    if(angka == 5){
+        cout<<"五 (Go)"<<endl;
+    }
+    if(angka == 6){
+        cout<<"六 (Roku)"<<endl;
+    }
+    if(angka == 7){
+        cout<<"七 (Nana)"<<endl;
+    }
+    if(angka == 8){
+        cout<<"八 (Hachi)"<<endl;
+    }
+    if(angka == 9){
+        cout<<"九 (Kyuu)"<<endl;
+    }
+    if(angka == 10){
+        cout<<"十 (Juu)"<<endl;
+    }
+}
